10d8d79fb9d95cfc489287b9ba01bd9f6f733a8b
Thelaw818 <93685225+Thelaw818@users.noreply.github.com>
2024-12-22 13:06:55 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 {
-  const section = document.getElementById(sectionId);
-  const icon = section.previousElementSibling.querySelector('.toggle-icon');
+    const section = document.getElementById(sectionId);
+    const icon = section.previousElementSibling.querySelector('.toggle-icon');
 
-  if (section.style.display === 'none' || !section.style.display) {
-    section.style.display = 'block'; // Show section
-    icon.textContent = '-'; // Change to minus icon
-  } else {
-    section.style.display = 'none'; // Hide section
-    icon.textContent = '+'; // Change to plus icon
-  }
+    if (section.style.display === 'none' || !section.style.display) {
+        section.style.display = 'block'; // Show section
+        icon.classList.remove('collapsed');
+        icon.classList.add('expanded');
+    } else {
+        section.style.display = 'none'; // Hide section
+        icon.classList.remove('expanded');
+        icon.classList.add('collapsed');
+    }
 }
